b07f9e3254fcb5e209bd4da7a9aebbc6fe82d2a9
omanar <omanar@student.1337.ma>
2023-08-15 10:08:49 +0100

    Starting...

---
+++ 42-UbuntuCleaner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 42 Ubuntu Cleaner Script
+# Author: Oussama Manar
+# login: omanar
+# Description: Optimizes and cleans your Ubuntu system for more free spaces.
+
+# Perform cleaning tasks
+echo -e "Cleaning and optimizing your system...\n"
+/bin/rm -rf ~/.cache/.thumbnails/* &> /dev/null
+/bin/rm -rf ~/thumbnails/* &> /dev/null
+/bin/rm -rf ~/.local/share/Trash/* &> /dev/null
+/bin/rm -rf ~/tmp/* &> /dev/null
+/bin/rm -rf ~/.cache/system-cache/* &> /dev/null
+
+
+echo -e "\033[32mCleaning completed!\033[0m"
+
+# End of script
